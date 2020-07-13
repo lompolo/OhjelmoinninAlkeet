95e095fcc3e4cec183df0f2527fbe67c820a6460
Tero Lompolo <tero.lompolo@gmail.com>
2020-07-13 21:40:20 +0300

    päivietty spiraali tiedostosta

--- spiraali_suoraan_tiedostosta.py
+++ spiraali_suoraan_tiedostosta.py
@@ -11,7 +11,20 @@ def piirra_spiraali(vari, kaarien_lkm, sade, kasvu, viivan_paksuus=1):
 
 
 def piirra_tiedostosta(tiedoston_nimi):
-    pass
+    try:
+        with open(tiedoston_nimi) as tiedosto:
+            for rivi in tiedosto.readlines():
+                osat = rivi.split(',')
+                for i, osa in enumerate(osat):
+                    osat[i] = osa.strip()
+                vari = osat[0]
+                kaaret = int(osat[1])
+                sade = int(osat[2])
+                kasvu = float(osat[3])
+                viiva = int(osat[4])
+                piirra_spiraali(vari, kaaret, sade, kasvu, viiva)
+    except IOError:
+        print("Kohdetiedostoa ei voitu avata. Tallennus epäonnistui")
 
 
 piirra_tiedostosta('spiraali.txt')
